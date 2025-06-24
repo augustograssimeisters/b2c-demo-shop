3b1ec00fcc85868a5423c28fb276c61c90278ce8
Augusto Grassi <augusto.grassi@meisters.solutions>
2025-06-24 20:03:39 -0300

    isso ae
    Signed-off-by: Augusto Grassi <augusto.grassi@meisters.solutions>

--- src/Pyz/Zed/AntelopeLocationSearch/Business/AntelopeLocationSearchFacadeInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Pyz\Zed\AntelopeLocationSearch\Business;
-
-interface AntelopeLocationSearchFacadeInterface
-{
-    public function publish(array $antelopeLocationIds): void;
-}
--- src/Pyz/Zed/AntelopeLocationSearch/Communication/AntelopeLocationSearchCommunicationFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Pyz\Zed\AntelopeLocationSearch\Communication;
-
-use Spryker\Zed\Kernel\Communication\AbstractCommunicationFactory;
-
-class AntelopeLocationSearchCommunicationFactory extends AbstractCommunicationFactory
-{
-}
